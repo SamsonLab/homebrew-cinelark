bd991ae115cb54ab77adf75cc7b0b36074a9f218
SamsonLab Release <release@samsonlab.com>
2026-08-26 09:59:29 +0800

    Update cask to v0.1.9

--- Casks/cinelark.rb
+++ Casks/cinelark.rb
@@ -1,6 +1,6 @@
 cask "cinelark" do
-  version "0.1.8"
-  sha256 "22690dd3b8995d0c11f0cad9afe37a8c35ea492188d1fdb191309e2c1688beb4"
+  version "0.1.9"
+  sha256 "c6720dba5ca72c615e43759720a6080f9be898b5dbd07afbc5d2619624b79c06"
 
   url "https://github.com/SamsonLab/cinelark/releases/download/v#{version}/CineLark_#{version}_universal.dmg"
   name "CineLark"
